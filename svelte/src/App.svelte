5fe35bfb511c95bcd039b316e7475fbfdbc4b645
Suomi228 <bixep345@gmail.com>
2025-02-18 15:15:42 +0300

    feat: header

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -12,43 +12,41 @@
   }
 </script>
 
-<body>
-  <header class="header">
-    <nav class="menu menu--top">
+<header class="header">
+  <nav class="menu menu--top">
+    <ul class="menu__list">
+      {#each menuTop as item (item)}
+        <li class="menu__item {selected === item ? 'active' : ''}">
+          <a
+            href="#"
+            class="menu__link {selected === item ? 'active' : ''}"
+            onclick={() => selectItem(item)}
+          >
+            {item}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
+  <div class="header__inner">
+    <div class="header__white-left">
+      <div class="header__burger"></div>
+      <div class="header__logo"></div>
+      <div class="header__title">
+        Система управления проектной документацией проекта ВСМ-1
+      </div>
+    </div>
+    <nav class="menu menu--bottom">
       <ul class="menu__list">
-        <li class="menu__item">
-          <a href="#" class="menu__link">Главная страница</a>
-        </li>
-        <li class="menu__item"><a href="#" class="menu__link">Карта</a></li>
-        <li class="menu__item">
-          <a href="#" class="menu__link">Высокоскоростной электропоезд</a>
-        </li>
-        <li class="menu__item">
-          <a href="#" class="menu__link">Финансовая модель</a>
-        </li>
-        <li class="menu__item"><a href="#" class="menu__link">Архив</a></li>
+        <li class="menu__item"><a href="#" class="menu__link">Объекты</a></li>
+        <li class="menu__item"><a href="#" class="menu__link">Регионы</a></li>
+        <li class="menu__item"><a href="#" class="menu__link">Этапы</a></li>
+        <li class="menu__item"><a href="#" class="menu__link">Слои</a></li>
       </ul>
     </nav>
-    <div class="header__inner">
-      <div class="header__white-left">
-        <div class="header__burger"></div>
-        <div class="header__logo"></div>
-        <div class="header__title">
-          Система управления проектной документацией проекта ВСМ-1
-        </div>
-      </div>
-      <nav class="menu menu--bottom">
-        <ul class="menu__list">
-          <li class="menu__item"><a href="#" class="menu__link">Объекты</a></li>
-          <li class="menu__item"><a href="#" class="menu__link">Регионы</a></li>
-          <li class="menu__item"><a href="#" class="menu__link">Этапы</a></li>
-          <li class="menu__item"><a href="#" class="menu__link">Слои</a></li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-  <main></main>
-</body>
+  </div>
+</header>
+<main></main>
 
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap");
