c5d51b76d2978ddc995ffaf0bd71b0905db47fab
Suomi228 <bixep345@gmail.com>
2025-02-18 23:32:44 +0300

    feat: map gallery

--- svelte/src/lib/Map.svelte
+++ svelte/src/lib/Map.svelte
@@ -2,37 +2,44 @@
   import { onMount, onDestroy } from "svelte";
   import { Map } from "maplibre-gl";
   import "maplibre-gl/dist/maplibre-gl.css";
-  import lineGeoJson from "../line.json"
-
+  import lineGeoJson from "../line.json";
   let map;
   let mapContainer;
-
+  let showImages = false;
   const apiKey = import.meta.env.VITE_MAP_LIBRE_API_KEY;
+  const images = [
+    "/Group 232.png",
+    "/Group 233.png",
+    "/Group 234.png",
+    "/Group 235.png",
+  ];
 
   onMount(() => {
-
     map = new Map({
       container: mapContainer,
       style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${apiKey}`,
-      center: [36.450429,56.595394],
+      center: [36.450429, 56.595394],
       zoom: 10.6,
-      attributionControl: false
+      attributionControl: false,
     });
 
-    map.on('load', () => {
-      map.addSource('line-source', {
-        type: 'geojson',
+    map.on("load", () => {
+      map.addSource("line-source", {
+        type: "geojson",
         data: lineGeoJson,
       });
 
       map.addLayer({
-        id: 'line-layer',
-        type: 'line',
-        source: 'line-source',  
+        id: "line-layer",
+        type: "line",
+        source: "line-source",
         paint: {
-          'line-color': '#FF5733',
-          'line-width': 4,
+          "line-color": "#FF5733",
+          "line-width": 4,
         },
+      });
+      map.on("click", "line-layer", () => {
+        showImages = !showImages;
       });
     });
   });
@@ -42,35 +49,26 @@
   });
 </script>
 
-<div class="map-wrap">
-  <a href="https://www.maptiler.com" class="watermark"
-    ><img
+<div class="map">
+  <a href="https://www.maptiler.com" class="map__watermark">
+    <img
       src="https://api.maptiler.com/resources/logo.svg"
       alt="MapTiler logo"
-    /></a
-  >
-  <div class="map" bind:this={mapContainer}></div>
+    />
+  </a>
+  <div class="map__container" bind:this={mapContainer}></div>
 </div>
 
+{#if showImages}
+  <div class="gallery">
+    {#each images as img}
+      <div class="gallery__item">
+        <img src={img} alt="Train route" class="gallery__image" />
+      </div>
+    {/each}
+  </div>
+{/if}
+
 <style>
-  .map-wrap {
-    position: relative;
-    width: 100%;
-    height: calc(
-      100vh - 77px
-    );
-  }
-
-  .map {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-  }
-
-  .watermark {
-    position: absolute;
-    left: 10px;
-    bottom: 10px;
-    z-index: 999;
-  }
+  @import "../styles/map.css";
 </style>
